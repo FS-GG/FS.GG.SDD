94fbe974977ef67f2b787d5a1ee436c21a46862a
EHotwagner <ehotwagner@gmail.com>
2026-08-01 12:06:22 +0200

    test: retain console lifecycle evidence
    FSGG-Worker: curlew-fdc4

--- tests/fixtures/polyglot-lifecycle/no-npm-console/Program.fs
+++ tests/fixtures/polyglot-lifecycle/no-npm-console/Program.fs
@@ -1,1 +1,10 @@
+open System.IO
+
+Directory.CreateDirectory("results") |> ignore
+
+File.WriteAllText(
+    "results/console.junit.xml",
+    "<testsuite tests=\"1\"><testcase name=\"no-npm-console\" /></testsuite>"
+)
+
 printfn "no-npm console fixture"
